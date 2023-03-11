a45ef75ac18e44626011a1c7132a7ca7e797ce99
benha <ben.hasbani2@gmail.com>
2023-03-11 08:55:09 +0200

    prepare csv

---
+++ prep_csv.py
@@ -0,0 +1,31 @@
+import os
+import re
+import csv
+
+class gen_csv():
+    def __init__(self, root):
+        self.root = root
+
+    def prep_csv(self):
+        with open('dataset.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            first_row = ['relative path', 'SNR']
+            writer.writerow(first_row)
+            for root, _, files in os.walk(self.root):
+                for file in files:
+                    if file.endswith('.pickle'):
+                        dir = file.split('_')[0]
+                        if(re.search(r'SNR(\d+)_db', file)):
+                            SNR = re.search(r'SNR(\d+)_db', file).group(1)
+                        elif('SNR-' in file):
+                            SNR = '-' + file.split('-')[1].split('_')[0]
+                        else:
+                            SNR = 'clean'
+                        relative_path = f"/{dir}/{file}"
+                        curr_row = [relative_path, SNR]
+                        writer.writerow(curr_row)
+
+if __name__ == "__main__":
+    dir_root = f"/dsi/scratch/from_netapp/users/hazbanb/dataset/musicnet/train_data_split"
+    csv_ins = gen_csv(dir_root)
+    csv_ins.prep_csv()
